dc232aba044e30288893633615093a325deb9bbd
fabriziocava <fabrizio.cava88@gmail.com>
2013-11-06 12:40:38 +0100

    Root class modify

--- VacuumCleaner_Editor/src/core/Room.java
+++ VacuumCleaner_Editor/src/core/Room.java
@@ -11,6 +11,8 @@ public class Room {
 	public static final int DEFAULT_SIZE = 10;
 	public static final int CLEAN = 0;
 	public static final int DIRTY = 1;
+	public static final int WALL = 2;
+	public static final int BASE = -1;
 	
 	private int[][] cell;
 	
@@ -27,6 +29,15 @@ public class Room {
 
 	public void setCell(int i, int j, int state) {
 		cell[i][j]=state;
+	}
+	
+	public boolean addBase(int x, int y) {
+		for(int i=0; i<DEFAULT_SIZE; i++)
+			for(int j=0; j<DEFAULT_SIZE; j++)
+				if(cell[i][j]==-1)
+					return false;
+		cell[x][y]=-1;
+		return true;
 	}
 	
 		
